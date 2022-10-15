61419955a914c3e44368e234837be057acb96d44
turki Albalawi <talbalawi@channels.com.sa>
2022-10-16 01:13:25 +0300

    a

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/<name-of-app>'));
-app.use(express.static(path.join(__dirname, 'dist','app_name')));
+app.use(express.static(path.join(__dirname, 'dist','front-end')));
 app.get('/*', function(req,res) {
     
 // res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
+res.sendFile(path.join(__dirname,'dist','front-end','index.html'));
 });
 
 // Start the app by listening on the default Heroku port
